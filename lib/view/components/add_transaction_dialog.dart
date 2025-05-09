da3d27408c69612a4af465e6adb992d28aa14236
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-08 22:26:26 -0500

    installed currency_text_input_formatter: ^2.3.0

--- lib/view/components/add_transaction_dialog.dart
+++ lib/view/components/add_transaction_dialog.dart
@@ -1,10 +1,69 @@
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AddTransactionDialog extends StatelessWidget {
+class AddTransactionDialog extends StatefulWidget {
   const AddTransactionDialog({super.key});
 
   @override
+  State<AddTransactionDialog> createState() => _AddTransactionDialogState();
+}
+
+class _AddTransactionDialogState extends State<AddTransactionDialog> {
+  int? typeIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(child: const Text('New Transaction'));
+    final textTheme = Theme.of(context).textTheme;
+
+    return SizedBox(
+      height: 680,
+      width: double.infinity,
+      child: Column(
+        children: [
+          Container(
+            height: 6,
+            width: 48,
+            margin: EdgeInsets.only(top: 12),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(3),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'New Transaction',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.teal,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          CupertinoSlidingSegmentedControl(
+            groupValue: typeIndex,
+            children: const {0: Text('Income'), 1: Text('Expense')},
+            onValueChanged: (value) {
+              setState(() {});
+              typeIndex = value;
+            },
+          ),
+          SizedBox(height: 20),
+          Text(
+            'AMOUNT',
+            style: textTheme.bodySmall!.copyWith(color: Colors.teal),
+          ),
+          TextField(
+            inputFormatters: [
+              CurrencyTextInputFormatter.currency(symbol: '\$'),
+            ],
+            textAlign: TextAlign.center,
+            decoration: InputDecoration.collapsed(hintText: '\$ 0.00'),
+            keyboardType: TextInputType.number,
+          ),
+        ],
+      ),
+    );
   }
 }
