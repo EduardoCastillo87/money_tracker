4ea1dfbddd8dd52b28be7f29966857dddd0dc18b
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-08 21:16:51 -0500

    created AddTransactionDialog

---
+++ lib/view/components/add_transaction_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddTransactionDialog extends StatelessWidget {
+  const AddTransactionDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: const Text('New Transaction'));
+  }
+}
