48eea5905a2a11fe80e43574484fd990f811c32b
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-08 20:13:53 -0500

    add trasaction list widget ListTile Income and Expense

--- lib/view/components/transaction_list.dart
+++ lib/view/components/transaction_list.dart
@@ -6,7 +6,25 @@ class Transactions_list extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(decoration: BoxDecoration(color: Colors.white)),
+      child: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.attach_money, color: Colors.teal),
+              title: const Text('Income'),
+              subtitle: const Text('You got \$ 1,000.00'),
+              trailing: const Text('\$ 1,000.00'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.attach_money, color: Colors.red),
+              title: const Text('Expense'),
+              subtitle: const Text('You spent \$ 500.00'),
+              trailing: const Text('- \$ 500.00'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
